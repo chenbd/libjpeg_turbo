a64152c834a25a5838fefc8e4f01808bc055082d
Android Chromium Automerger <chromium-automerger@android>
2014-10-19 22:50:11 +0000

    Update makefiles after merge of Chromium at 89b463ddd92b
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: Ie00e1b8610c67379fcdc5d3bfd3200ba0d05752f

--- libjpeg.target.darwin-x86_64.mk
+++ libjpeg.target.darwin-x86_64.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 LOCAL_MODULE := third_party_libjpeg_turbo_libjpeg_gyp
 LOCAL_MODULE_SUFFIX := .a
 LOCAL_MODULE_TARGET_ARCH := $(TARGET_$(GYP_VAR_PREFIX)ARCH)
+LOCAL_SDK_VERSION := 19
 gyp_intermediate_dir := $(call local-intermediates-dir,,$(GYP_VAR_PREFIX))
 gyp_shared_intermediate_dir := $(call intermediates-dir-for,GYP,shared,,,$(GYP_VAR_PREFIX))
 
@@ -314,10 +315,7 @@ MY_DEFS_Debug := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Debug := \
 	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/libjpeg_turbo \
-	$(PWD)/frameworks/wilhelm/include \
-	$(PWD)/bionic \
-	$(PWD)/external/stlport/stlport
+	$(LOCAL_PATH)/third_party/libjpeg_turbo
 
 
 # Flags passed to only C++ (and not C) files.
@@ -409,10 +407,7 @@ MY_DEFS_Release := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Release := \
 	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/libjpeg_turbo \
-	$(PWD)/frameworks/wilhelm/include \
-	$(PWD)/bionic \
-	$(PWD)/external/stlport/stlport
+	$(LOCAL_PATH)/third_party/libjpeg_turbo
 
 
 # Flags passed to only C++ (and not C) files.
@@ -435,13 +430,9 @@ LOCAL_C_INCLUDES := $(GYP_COPIED_SOURCE_ORIGIN_DIRS) $(LOCAL_C_INCLUDES_$(GYP_CO
 LOCAL_CPPFLAGS := $(LOCAL_CPPFLAGS_$(GYP_CONFIGURATION))
 LOCAL_ASFLAGS := $(LOCAL_CFLAGS)
 ### Rules for final target.
-
-LOCAL_SHARED_LIBRARIES := \
-	libstlport \
-	libdl
-
 ### Set directly by aosp_build_settings.
 LOCAL_CLANG := false
+LOCAL_NDK_STL_VARIANT := stlport_static
 
 # Add target alias to "gyp_all_modules" target.
 .PHONY: gyp_all_modules
